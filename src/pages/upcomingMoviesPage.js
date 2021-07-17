e56f39145273a4dfc6b3841b928485808e540f46
Emma <kidneyemma@gmail.com>
2021-07-17 15:26:17 +0100

    New button for upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -4,6 +4,8 @@ import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import { getUpcomingMovies } from "../api/tmdb-api";
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+
 const UpcomingMoviesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies)
   if (isLoading) {
@@ -20,7 +22,7 @@ const UpcomingMoviesPage = (props) => {
       title='Upcoming Movies'
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <PlaylistAddIcon movie={movie} />
       }}
     />
   );
